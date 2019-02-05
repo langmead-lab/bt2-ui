5dfad8c4e392ef9dee382bfc108548e1a53d84ff
Rone Charles <rone_charles@fastmail.com>
2019-02-04 20:12:01 -0500

    Add grace period between killing and starting container

--- restart.sh
+++ restart.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 CID=`docker ps | grep shiny | awk '{ print $1 }'`
-docker kill $CID && docker run --privileged --name bt2-ui --rm -p 3838:3838 -d $* benlangmead/bt2-ui
+docker kill $CID && sleep 1; docker run --privileged --name bt2-ui --rm -p 3838:3838 -d $* benlangmead/bt2-ui
