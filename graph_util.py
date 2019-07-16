fa9182379c66169463897aadd1efb296e85583be
Maxwell Fleming <mjfleming99@gmail.com>
2019-07-16 18:42:56 +0200

    Better parces SAM files, more resistant to odd output.

--- graph_util.py
+++ graph_util.py
@@ -63,6 +63,7 @@ def make_data_for_box_plot(read_quality):
     return data
 
 def parseString(txt):
+    spliter = re.compile('\n+')
     readnumber = re.compile('[r]+\d+')
     line_spliter = re.compile('\t+')
     colon_spliter = re.compile(':')
@@ -75,29 +76,31 @@ def parseString(txt):
     #TODO throw an error/ warning if the readSize is greater than the number
     #of lines in the file.
 
-    lines = txt.splitlines()
+    lines = spliter.split(txt)
 
     #Itterating though everyline
     for i in range(len(lines) - 1):
-        get_match_score = True
-        #Splitting the lines into whitespace
-        subline = line_spliter.split(lines[i])
+        try:
+            get_match_score = True
+            #Splitting the lines into whitespace
+            subline = line_spliter.split(lines[i])
+            if (int(subline[1]) % 2 == 0):
+                unmatched_reads += 1
+                get_match_score = False
+            elif ((int(subline[1]) >> 4) % 2 == 0):
+                forward_reads += 1
+            else:
+                reverse_reads += 1
 
-        if (subline[1] == '0'):
-            forward_reads += 1
-        elif (subline[1] =='16'):
-            reverse_reads += 1
-        else:
-            unmatched_reads += 1
-            get_match_score = False
+            for j in range(len(subline[10])):
+                while(len(read_quality) < len(subline[10])):
+                    read_quality.append([])
+                read_quality[j].append(subline[10][j])
 
-        for j in range(len(subline[10])):
-            while(len(read_quality) < len(subline[10])):
-                read_quality.append([])
-            read_quality[j].append(subline[10][j])
-
-        if (get_match_score):
-            match_scores.append(int(colon_spliter.split(subline[11])[2]))
+            if (get_match_score):
+                match_scores.append(int(colon_spliter.split(subline[11])[2]))
+        except:
+            print("Invalid Line")
     read_quality = read_quality_converter(read_quality)
     return (forward_reads, reverse_reads, unmatched_reads, read_quality, match_scores)
 
