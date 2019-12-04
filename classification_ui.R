2a79bb6253796cafa65e00915d2219b7fda8cec6
Maxwell Fleming <mfleming@Maxwells-MacBook-Pro.local>
2019-12-04 12:46:18 -0500

    Finished UI for classification.

--- classification_ui.R
+++ classification_ui.R
@@ -6,17 +6,7 @@ classification_tab <- fluidPage(
   titlePanel("Welcome to the Classification App!"),
   sidebarLayout(
     sidebarPanel(
-      p("This app is designed to help you help classify your accessions"),
-      fluidRow(
-        id = "selectClassificationIndex",
-        selectizeInput(
-          #This needs to be changed so its only human chromosomes
-          "index3",
-          label = "Bowtie2 Index to be used",
-          options = list(placeholder = "Select Index"),
-          choices = NULL
-        )
-      ),
+      p("This app is designed to help you help classify your accessions. We are using data for GRCh38, please us an accession that matches in order to get useful information."),
       fluidRow(
         id = "sraAccessionClassification",
         textInput("index5", "Sequence Read Archive", "")
@@ -24,14 +14,46 @@ classification_tab <- fluidPage(
       fluidRow(
         bsButton(
           "classificationSubmit",
-          "Submit",
+          "Classify",
+          stype = "primary",
+          icon = icon("play")
+        )
+      ),
+      br(),
+      fluidRow(
+        p("Compare your accession to our dataset"),
+        selectInput(
+          "index5",
+          "Feature you want on the x axis",
+          c("Gene Annotation Percent", "Average Read Length", "Read Frequency", "Possition Differece STD", "Possition Difference Mean", "Number of Chromosomes", "Largest Position Difference", "Smallest Position Difference"),
+          selected = "Gene Annotation Percent"
+        ),
+        selectInput(
+          "index6",
+          "Feature you want on the y axis",
+          c("Gene Annotation Percent", "Average Read Length", "Read Frequency", "Possition Differece STD", "Possition Difference Mean", "Number of Chromosomes", "Largest Position Difference", "Smallest Position Difference"),
+          selected = "Average Read Length"
+        )
+      ),
+      fluidRow(
+        bsButton(
+          "comparisonSubmit",
+          "Compare",
           stype = "primary",
           icon = icon("play")
         )
       )
     ),
     mainPanel(
-      textOutput("This is where the graphs will go")
+      p("This is where the graphs will go"),
+      conditionalPanel(
+        condition = "output.classification_update === true",
+        p("This is our confidence rating for your accesssion")
+      ),
+      conditionalPanel(
+        condition = "output.comparison_update === true",
+        p("This is how your accession compares to our dataset")
+      )
     )
   )
 )
